62eb3d34ae07e8ee7af059da620510e5abe66f0d
maki <xiefeiyang.cat@bytedance.com>
2020-04-10 16:10:27 +0800

    add test server

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 
+	"maki.io/demo/shaver/service"
 	"maki.io/demo/shaver/util"
 )
 
@@ -17,8 +18,25 @@ func sqrt(x float64) float64 {
 }
 
 func main() {
+	// test part
 	s := util.ReverseString("go is weird")
 	i := sqrt(16)
 	fmt.Println(s)
 	fmt.Println(i)
+
+	// the server part
+	name := "testPage"
+	p1 := &service.Page{Title: name, Body: []byte("This is a sample page")}
+	save_err := p1.Save()
+	if save_err != nil {
+		fmt.Printf("step 1: page save error %v", save_err)
+	} else {
+		p2, load_err := service.LoadPage(name)
+		if load_err != nil {
+			fmt.Printf("step 2: page load error %v", load_err)
+		} else {
+			fmt.Println(string(p2.Body))
+		}
+	}
+
 }
